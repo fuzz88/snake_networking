6fa6468205670eac59f8b5f18893ae050cf2aea8
fuzz88 <ivan@oschepkov.ru>
2025-02-07 16:04:25 +0800

    refactor grid; add snake

--- main.c
+++ main.c
@@ -3,20 +3,43 @@
 
 #define SCREEN_WIDTH    800
 #define SCREEN_HEIGHT   600
-#define CELL_WIDHT      20
+#define CELL_WIDTH      20
 #define CELL_HEIGHT     20
 
-#define BG_COLOR        0x131313FF
-#define GRID_COLOR      0xAFAFAFFF
 
-void draw_grid(){
-    Color line_color = (Color)GetColor(GRID_COLOR);
-    for (int cell_x = 0; cell_x < SCREEN_WIDTH / CELL_WIDHT; ++cell_x) {
-        DrawLine(cell_x * CELL_WIDHT, 0, cell_x * CELL_WIDHT, SCREEN_HEIGHT, line_color);
+#define GRID_MAX_X      SCREEN_WIDTH / CELL_WIDTH 
+#define GRID_MAX_Y      SCREEN_HEIGHT / CELL_HEIGHT
+#define GRID_MIN_X      1
+#define GRID_MIN_Y      1
+
+#define MAX_X           GRID_MAX_X - 3 
+#define MAX_Y           GRID_MAX_Y - 3 
+#define MIN_X           0
+#define MIN_Y           0
+
+#define BG_COLOR        (Color){0x13, 0x13, 0x13, 0xFF}
+#define GRID_COLOR      (Color){0xAF, 0xAF, 0xAF, 0xFF}
+#define SNAKE_COLOR     (Color){0x10, 0x9F, 0x10, 0xFF}
+
+void draw_grid() {
+    for (int cell_x = GRID_MIN_X; cell_x < GRID_MAX_X; ++cell_x) {
+        DrawLine(cell_x * CELL_WIDTH, CELL_WIDTH, cell_x * CELL_WIDTH, SCREEN_HEIGHT - CELL_HEIGHT, GRID_COLOR);
     }
-    for (int cell_y = 0; cell_y < SCREEN_HEIGHT / CELL_HEIGHT; ++cell_y) {
-        DrawLine(0, cell_y * CELL_HEIGHT, SCREEN_WIDTH, cell_y * CELL_HEIGHT, line_color);                
+    for (int cell_y = GRID_MIN_Y; cell_y < GRID_MAX_Y; ++cell_y) {
+        DrawLine(CELL_HEIGHT, cell_y * CELL_HEIGHT, SCREEN_WIDTH - CELL_WIDTH, cell_y * CELL_HEIGHT, GRID_COLOR);                
     }
+}
+
+static inline void fill_cell(int x, int y, Color fill_color) {
+    DrawRectangle((x + 1) * CELL_WIDTH, (y + 1) * CELL_HEIGHT, CELL_WIDTH - 1, CELL_HEIGHT - 1, fill_color);
+}
+
+void draw_snake_segment(int x, int y) {
+    fill_cell(x, y, SNAKE_COLOR);
+}
+
+void clear_snake_segment(int x, int y) {
+    fill_cell(x, y, BG_COLOR);
 }
 
 int main(void)
@@ -26,9 +49,15 @@ int main(void)
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground((Color)GetColor(BG_COLOR));
+        ClearBackground(BG_COLOR);
         draw_grid();
         EndDrawing();
+        draw_snake_segment(0, 0);
+        draw_snake_segment(MAX_X, MAX_Y);
+        draw_snake_segment(0, MAX_Y);
+        draw_snake_segment(MAX_X, 0);
+
+        clear_snake_segment(MAX_X, MAX_Y);
     }
     
     CloseWindow();
